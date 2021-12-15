a59ea2827a373ba114b20e33ae8b523a5d80f1ed
Binhui Chen Zhou <779799@unizar.es>
2021-12-15 17:20:39 +0100

    Include QRCode Url in Front-end

--- app/src/main/resources/static/js/app.js
+++ app/src/main/resources/static/js/app.js
@@ -8,16 +8,30 @@ $(document).ready(
                     url : "/api/link",
                     data : $(this).serialize(),
                     success : function(msg, status, request) {
-                        $("#result").html(
-                            "<div class='alert alert-success lead'><a target='_blank' href='"
-                            + request.getResponseHeader('Location')
-                            + "'>"
-                            + request.getResponseHeader('Location')
-                            + "</a></div>");
+                            var qrComponent = "";
+                             if(msg.qr != null) qrComponent =
+                                    "<br/><p class='card-text'> QRCode at <a target='_blank' href='"
+                                    + msg.qr
+                                    + "'>"
+                                    + msg.qr
+                                    + "</a></p>"
+                                    + "</div>";
+                            $("#result").html(
+                                "<div class='card text-center border-primary mx-auto' "
+                                +"style='max-width: 70%; background-color:#E0FFFF;'>"
+                                + "<div class='card-body'>"
+                                + "<h6 class='card-title'><a target='_blank' href='"
+                                + request.getResponseHeader('Location')
+                                + "'>"
+                                + request.getResponseHeader('Location')
+                                + "</a></h6>"
+                                + qrComponent
+                                + "</div>"
+                                );
                     },
                     error : function() {
                         $("#result").html(
-                            "<div class='alert alert-danger lead'>ERROR</div>");
+                            "<div class='alert alert-danger lead mx-auto' style='max-width: 70%;'>ERROR</div>");
                     }
                 });
             });
